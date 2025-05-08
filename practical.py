0173ea1aea8d24e8913a99635736f2f0567a62a3
mrunmayeeraut <mrun2004@gmail.com>
2025-05-08 11:05:12 +0530

    Add files via upload

---
+++ practical.py
@@ -0,0 +1,4 @@
+print("Hello World!!!")
+print("I am Mrunmayee Raut, T400530648")
+print("Performing LP-II Practical of cloud computing")
+print("Problem statement: Installation and Configure Google App Engine")
